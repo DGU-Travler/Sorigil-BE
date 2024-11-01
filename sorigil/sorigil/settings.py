f750d1fb4a6da5139a19623ffd25aa98dfdadda8
Gnoddeno <gnoddeno@gnoddenoui-macbookair.local>
2024-11-01 13:51:40 +0900

    init setting

--- sorigil/sorigil/settings.py
+++ sorigil/sorigil/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yw%m0b@uj)tmh6(dq9kn8-ihxhsa@p!@kxh614=2s+dhvl5&1b'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-ALLOWED_HOSTS = []
+SECRET_KEY = env('SECRET_KEY')
+
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
